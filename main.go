624cabd7b0fb14ccfa1d8bb7d3d898b325c424ab
Sergey <chapal@inbox.ru>
2019-09-16 16:14:57 +0300

    working

--- main.go
+++ main.go
@@ -1,18 +1,48 @@
 package main
 
 import (
+	"fmt"
+	"github.com/fluent/fluent-logger-golang/fluent"
 	log "github.com/sirupsen/logrus"
 	"net/http"
 )
 
+type Student struct {
+	Name string `msg:"name"`
+	IdNo int    `msg:"id_no"`
+	Age int     `msg:"age"`
+}
+
 func init()  {
-	log.SetFormatter(&log.JSONFormatter{})
+	//log.SetFormatter(&log.JSONFormatter{})
 }
 func main() {
+
+	logger,err :=fluent.New(fluent.Config{FluentHost:"fluentd", FluentPort:24224})
+	fmt.Println(logger.BufferLimit)
+	if err != nil{
+		fmt.Println("error creating fluentd instance", err)
+		return
+	}
+
+	defer logger.Close()
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Info("Send info")
-		log.Error("Send Error")
-		w.Write([]byte("send logs to stdout and stderr"))
+		log.Info("test")
+
+		tag := "fluentd-log-demo"
+		data := map[string]interface{}{
+			"name": "John",
+			"id_no": 1234,
+			"age": 25,
+		}
+		err := logger.Post(tag,data)
+
+		if err != nil {
+			fmt.Printf("Got error: %v", err)
+		}
+
+		_, _  = w.Write([]byte("OK"))
 	})
 
 	log.Fatal(http.ListenAndServe(":5002", nil))
