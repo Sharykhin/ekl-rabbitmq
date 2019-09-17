0d3c568a3b6a4214bf0437b448b43d2e4d1224e2
Sergey <chapal@inbox.ru>
2019-09-17 14:46:49 +0300

    working on better index

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"github.com/fluent/fluent-logger-golang/fluent"
 	log "github.com/sirupsen/logrus"
 	"net/http"
 )
@@ -14,35 +12,39 @@ type Student struct {
 }
 
 func init()  {
-	//log.SetFormatter(&log.JSONFormatter{})
+	log.SetFormatter(&log.JSONFormatter{})
 }
 func main() {
 
-	logger,err :=fluent.New(fluent.Config{FluentHost:"fluentd", FluentPort:24224})
-	fmt.Println(logger.BufferLimit)
-	if err != nil{
-		fmt.Println("error creating fluentd instance", err)
-		return
-	}
-
-	defer logger.Close()
+	//logger,err :=fluent.New(fluent.Config{FluentHost:"fluentd", FluentPort:24224})
+	//fmt.Println(logger.BufferLimit)
+	//if err != nil{
+	//	fmt.Println("error creating fluentd instance", err)
+	//	return
+	//}
+	//
+	//defer logger.Close()
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Info("test")
-
-		tag := "fluentd-log-demo"
-		data := map[string]interface{}{
-			"name": "John",
-			"id_no": 1234,
-			"age": 25,
-		}
-		err := logger.Post(tag,data)
-
-		if err != nil {
-			fmt.Printf("Got error: %v", err)
-		}
+		//_, err := os.Stdout.Write([]byte(`{"app": "simplebank", "key":"value"}`))
+		//if err !=nil {
+		//	panic(err)
+		//}
+		//tag := "fluentd-log-demo"
+		//data := map[string]interface{}{
+		//	"name": "John",
+		//	"id_no": 1234,
+		//	"age": 25,
+		//}
+		//err := logger.Post(tag,data)
+		//
+		//if err != nil {
+		//	fmt.Printf("Got error: %v", err)
+		//}
 
 		_, _  = w.Write([]byte("OK"))
+		return
 	})
 
 	log.Fatal(http.ListenAndServe(":5002", nil))
